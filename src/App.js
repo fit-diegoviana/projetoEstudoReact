700a58a711c70a1624d1ec9af78f6e2d11bdfcfe
Diego Viana <dtviana90@gmail.com>
2021-10-10 18:42:00 -0300

    feat: Firstcommit ProjectReact

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './App.css';
+import g from './assets/logo.svg'
+import Routes from "./routes";
+
+function App() {
+  return (
+<Routes/>
+);
+}
+
+export default App;
